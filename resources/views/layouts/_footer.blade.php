46d8a5de4eb7f42e7de31daf5c86088d85326107
Chark <15814277028@163.com>
2020-01-05 08:53:44 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <img class="brand-icon" src="https://cdn.learnku.com//uploads/communities/WtC3cPLHzMbKRSZnagU9.png!/both/20x20" alt="">
+    <a href="https://www.baidu.com">
+        刻意练习,每日精进
+    </a>
+
+    <div class="float-right">
+        <a href="/about"></a>
+    </div>
+</footer>
